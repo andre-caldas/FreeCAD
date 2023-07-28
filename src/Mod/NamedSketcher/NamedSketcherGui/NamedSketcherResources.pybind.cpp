f275075f0943c836534aae2c2b94133827b9cf89
André Caldas <andre.em.caldas@gmail.com>
2023-07-28 14:17:12 -0300

    Use pybind instead of legacy bindings.

---
+++ src/Mod/NamedSketcher/NamedSketcherGui/NamedSketcherResources.pybind.cpp
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/****************************************************************************
+ *                                                                          *
+ *   Copyright (c) 2023 André Caldas <andre.em.caldas@gmail.com>            *
+ *                                                                          *
+ *   This file is part of FreeCAD.                                          *
+ *                                                                          *
+ *   FreeCAD is free software: you can redistribute it and/or modify it     *
+ *   under the terms of the GNU Lesser General Public License as            *
+ *   published by the Free Software Foundation, either version 2.1 of the   *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ *   FreeCAD is distributed in the hope that it will be useful, but         *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with FreeCAD. If not, see                                *
+ *   <https://www.gnu.org/licenses/>.                                       *
+ *                                                                          *
+ ***************************************************************************/
+
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+#include <qglobal.h>
+
+static void q_init_resource()
+{
+    // This cannot be called from a namespace.
+    Q_INIT_RESOURCE(NamedSketcherResources);
+}
+
+namespace NamedSketcher
+{
+
+PYBIND11_MODULE(NamedSketcherResources, m)
+{
+    m.doc() = "Compiled resources and other bindings for NamedSketcher";
+    //init_ViewProviderPart(m);
+    q_init_resource();
+}
+
+} //namespace NamedSketcher
